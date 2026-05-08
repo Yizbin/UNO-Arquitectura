4ff1177351619464169f95b146e78957b45f1626
Yizbin <148611835+Yizbin@users.noreply.github.com>
2026-05-08 16:07:37 -0600

    filtro control

---
+++ UNO-Control/src/main/java/Conexiones/Control.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Conexiones;
+
+import DTOs.ConexionJugadorDTO;
+import interfaces.ContextoPipeline;
+import interfaces.IFiltro;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Abraham Coronel
+ * @param <T>
+ */
+public class Control<T> implements IFiltro<T,T> {
+
+    private final List<ConexionJugadorDTO> listaJugadores;
+
+    public Control() {
+        this.listaJugadores = new ArrayList<>();
+    }
+
+    public void registrarJugador(ConexionJugadorDTO conexion) {
+        this.listaJugadores.add(conexion);
+    }
+
+    public String getDireccion(int idJugador) {
+        for (ConexionJugadorDTO conexion : listaJugadores) {
+            if (conexion.getIdJugador() == idJugador) {
+                return conexion.getIp() + ":" + conexion.getPuerto();
+            }
+        }
+        return "Desconocido";
+    }
+
+
+    public List<ConexionJugadorDTO> getListaJugadores() {
+        return listaJugadores;
+    }
+
+    @Override
+    public ContextoPipeline<T> procesar(ContextoPipeline<T> contexto) throws Exception {
+        
+        T mensaje = contexto.getMensaje();
+        
+        if (mensaje == null) {
+            contexto.detenerConError("El mensaje llegó nulo al filtro de Control.");
+            return contexto;
+        }
+
+        //Logica del filtro
+        
+        return contexto;
+    }
+
+}
