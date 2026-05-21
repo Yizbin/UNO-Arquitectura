d459cc24eae080990ec5eab1e324b1370d7d1650
Yizbin <148611835+Yizbin@users.noreply.github.com>
2026-05-20 20:42:32 -0600

    x

--- UNO-Ensamblador/src/main/java/Main/Ensamblador.java
+++ UNO-Ensamblador/src/main/java/Main/Ensamblador.java
@@ -28,7 +28,7 @@ import pipeline.CoordinadorFiltros;
 
 public class Ensamblador {
 
-    private static final String IP_SERVIDOR = "127.0.0.1";
+    private static final String IP_SERVIDOR = "192.168.1.67";
     private static final int PUERTO_SERVIDOR = 5000;
     private static final int DESPLAZAMIENTO_PUERTO_CLIENTE = 1;
     private static final int ID_JUGADOR_ANFITRION = 1;
